03aca8ea510db6d96bcd58979a5d092069672fac
kpranat <pranatkheria@gmail.com>
2025-07-05 21:57:04 +0530

    sportify redirect change try

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ genai.configure(api_key=GEMINI_API_KEY)
 # Spotify config
 SPOTIFY_CLIENT_ID = 'ce0d57489375419f9a6401e35f26542b'
 SPOTIFY_CLIENT_SECRET = 'c1474458a85d48e9848fb1f893deee70'
-SPOTIFY_REDIRECT_URI = 'https://spacejam.onrender.com'
+SPOTIFY_REDIRECT_URI = 'https://spacejam.onrender.com/vibe.html'
 SCOPE = "streaming user-read-email user-read-private user-modify-playback-state user-read-playback-state"
 
 @app.route('/')
